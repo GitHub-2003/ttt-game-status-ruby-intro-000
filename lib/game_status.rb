326a3b736053e5869a5e1bc89e2a1067d2740280
MJU <marie.ju2012@outlook.com>
2020-02-12 07:19:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,4 +48,4 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
 board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-winner(board)
+puts(winner(board))
