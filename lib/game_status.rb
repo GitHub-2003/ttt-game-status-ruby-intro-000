61e60311c341d292e5da34ef2b622e4ff8f8e304
MJU <marie.ju2012@outlook.com>
2020-02-12 07:38:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,6 +51,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5
 
 board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
 # board = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
-puts(won?(board))
+junk = (won?(board))
+puts(junk)
 
 # puts(winner(board))
