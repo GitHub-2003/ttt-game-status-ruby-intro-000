27730d54f351af165106a86a1683073087720969
MJU <marie.ju2012@outlook.com>
2020-02-12 02:21:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,10 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def empty_board?(board)
+  
+end
+
 # 0 1 2
 # 3 4 5
 # 6 7 8
@@ -16,7 +20,7 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    if position_taken?(board, win_index_1) && position_1 == position_2 && position_2 == position_3
+    if (position_1 == "X" || position_1 = "O") && (position_1 == position_2) && (position_2 == position_3)
         return (win_combination) # return the win_combination indexes that won.
     else
       false
