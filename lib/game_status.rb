0ba872ab41fb81643fe8db0796e4453aa45f62d4
MJU <marie.ju2012@outlook.com>
2020-02-12 01:50:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if position_1 == position_2 && position_2 == position_3
       return (win_combination) # return the win_combination indexes that won.
     else
       false
@@ -26,4 +26,5 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
 board = ["O", "O", " ", "X", "X", "X", " ", " ", " "]
+board = ["O", " ", "X", "O", " ", "X", "O", " ", " "]
 print(won?(board))
