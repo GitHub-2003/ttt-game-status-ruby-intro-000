11c02fcbb401134367bad2e11a5ed2fe46ee1671
MJU <marie.ju2012@outlook.com>
2020-02-12 02:47:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,3 +31,8 @@ def won?(board)
   end
 end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+puts won?(board)
+board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+puts won?(board)
